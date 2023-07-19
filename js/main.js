c227d154161116aab5c53f5607ef82cf866704c5
prabhakaran <prabhakaranplkk@gmail.com>
2023-07-19 18:28:49 +0100

    digit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var arr=[39,646,786,93,356,34,77,12,657]
+var a=9;
+document.write("The Valu Numbers="+arr)
+
+for(let i=0;i<arr.length;i++)
+	
+{
+	if((arr[i]>9)&&(arr[i]<=99))
+	{
+		document.write("<br>")	
+	document.write(arr[i]);
+		
+		
+	}
+	
+} 
